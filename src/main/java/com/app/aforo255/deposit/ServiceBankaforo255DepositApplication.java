50c80f425b3cd7fbbdefc450b778efc4c98c0099
Crispin Quizo <cqm.cris@gmail.com>
2020-04-04 15:15:27 -0400

    first commit

---
+++ src/main/java/com/app/aforo255/deposit/ServiceBankaforo255DepositApplication.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.deposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankaforo255DepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBankaforo255DepositApplication.class, args);
+	}
+
+}
